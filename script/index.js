6c2dcdd7c3091c7ae8870f97be6d6355e6d5fe00
Elsayed Mersal <elsyed.ahmed1@gmail.com>
2023-04-01 00:55:09 +0200

    Add countdown timer function to events section

--- script/index.js
+++ script/index.js
@@ -19,3 +19,46 @@ function progressWithScroll() {
 progressWithScroll();
 
 /* ------------------------------------------------------------ */
+
+// Countdown Timer Function
+
+// The current time
+
+function countdown() {
+  let daysSpan = document.querySelector(".events .days");
+  let hoursSpan = document.querySelector(".events .hours");
+  let minutesSpan = document.querySelector(".events .minutes");
+  let secondsSpan = document.querySelector(".events .seconds");
+
+  let eventDate = new Date("April 21, 2023 23:59:59").getTime();
+
+  let counter = setInterval(() => {
+    let dateNow = new Date().getTime();
+
+    let timeDiff = eventDate - dateNow;
+
+    let eventDays = Math.floor(timeDiff / 1000 / 60 / 60 / 24);
+
+    let hoursInMS = timeDiff % (1000 * 60 * 60 * 24);
+    let eventHours = Math.floor(hoursInMS / (1000 * 60 * 60));
+
+    let minutesInMS = timeDiff % (1000 * 60 * 60);
+    let eventMinutes = Math.floor(minutesInMS / 1000 / 60);
+
+    let SecondsInMS = timeDiff % (1000 * 60);
+    let eventSeconds = Math.floor(SecondsInMS / 1000);
+
+    daysSpan.innerHTML = eventDays < 10 ? `0${eventDays}` : eventDays;
+    hoursSpan.innerHTML = eventHours < 10 ? `0${eventHours}` : eventHours;
+    minutesSpan.innerHTML =
+      eventMinutes < 10 ? `0${eventMinutes}` : eventMinutes;
+    secondsSpan.innerHTML =
+      eventSeconds < 10 ? `0${eventSeconds}` : eventSeconds;
+
+    if (eventDays == 0) {
+      clearInterval(counter);
+    }
+  }, 1000);
+}
+
+countdown();
